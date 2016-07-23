84e5de7a67e13c3a812bf5976359919465b9e7a2
Evan Battaglia <evan@moz.com>
2016-07-23 07:59:24 -0700

    more cleanups

--- test/manual/jobs/MyClass.js
+++ test/manual/jobs/MyClass.js
@@ -15,8 +15,8 @@ module.exports = {
     console.log(`Performing job with foo=${job.data.foo}... please wait!`);
     setTimeout(() => {
       console.log(`I'm out of here! Done with foo=${job.data.foo}`);
-      cb(new Error('wtf'));
-      // cb('wtf'); TODO TODO -- make this work
+      //cb(new Error('wtf'));
+      cb('wtf'); // TODO TODO -- make this work
     }, 1000);
   }
 }
