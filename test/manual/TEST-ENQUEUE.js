961c5f89c407d359427829f17af6384a695e4f33
Evan Battaglia <evan@moz.com>
2016-07-23 07:46:16 -0700

    cleanup

--- test/manual/TEST-ENQUEUE.js
+++ test/manual/TEST-ENQUEUE.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const client = new (require('./client').Client)();
+const qless = require('../../qless')
+
+const client = new qless.Client();
 client.queue('myqueue').put('MyClass', {foo: 'bar'}, {}, (err, res) => {
   if (err) {
     console.log("error: ", err, err.message);
