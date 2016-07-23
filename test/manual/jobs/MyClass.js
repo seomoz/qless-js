956576e55fcd2af7186bf2549c0fa5b54fb197f0
Evan Battaglia <evan@moz.com>
2016-07-23 12:17:05 -0700

    trying more stuff

--- test/manual/jobs/MyClass.js
+++ test/manual/jobs/MyClass.js
@@ -2,11 +2,18 @@
 
 const Promise = require('bluebird');
 
+function *doSomeStuff() {
+  console.log("doing some stuff");
+  yield Promise.delay(2000);
+  console.log("I've done some stuff");
+  throw new Error('wombat power!');
+  console.log("I've done some more stuff!");
+}
+
 function *perform(job) {
   console.log(`Performing job with foo=${job.data.foo}... please wait!`);
-  yield Promise.delay(2000);
+  yield doSomeStuff();
   console.log(`I'm out of here! Done with foo=${job.data.foo}`);
-  throw new Error('wombat power!');
 };
 
 module.exports = require('./generator2job')(perform);
