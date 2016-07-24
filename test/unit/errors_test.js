21161d920fa6947c72185da9807ac9993419f7d1
Evan Battaglia <evan@moz.com>
2016-07-23 22:38:39 -0700

    almost there

--- test/unit/errors_test.js
+++ test/unit/errors_test.js
@@ -1,23 +1,19 @@
 'use strict';
 
 describe('qless.errors', () => {
-  const jcfErr = new qless.errors.JobCantFail('foobar');
+  const lseErr = new qless.errors.LuaScriptError('foobar');
 
   it('can be tested with instanceof', () => {
-    expect(jcfErr instanceof qless.errors.JobCantFail).to.eql(true);
-    expect(jcfErr instanceof qless.errors.ClassNotFound).to.eql(false);
-  });
-
-  it('can be a subclass of a another error', () => {
-    expect(jcfErr instanceof qless.errors.LuaScriptError).to.eql(true);
+    expect(lseErr instanceof qless.errors.LuaScriptError).to.eql(true);
+    expect(lseErr instanceof qless.errors.ClassNotFound).to.eql(false);
   });
 
   it('has a descriptive name', () => {
-    expect(jcfErr.name).to.eql('qless.errors.JobCantFail');
+    expect(lseErr.name).to.eql('qless.errors.LuaScriptError');
   });
 
   it('has a message and stack', () => {
-    expect(jcfErr.message).to.eql('foobar');
-    expect(jcfErr.stack).to.match(/errors_test.js/);
+    expect(lseErr.message).to.eql('foobar');
+    expect(lseErr.stack).to.match(/errors_test.js/);
   });
 });
