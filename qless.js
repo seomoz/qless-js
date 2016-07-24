2b24f3b0565133eae4692d648fcdd09265a7a0ee
Evan Battaglia <evan@moz.com>
2016-07-23 21:56:21 -0700

    wip -- i broke something

--- qless.js
+++ qless.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   klassFinder: require('./lib/klass_finder'),
+  errors: require('./lib/errors'),
   Client: require('./lib/client').Client,
   SerialWorker: require('./lib/workers/serial').SerialWorker,
 };
